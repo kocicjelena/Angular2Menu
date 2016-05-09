a5616545dad0e767609610d220e1c62150b9e757
Jelena Kocic <kocicjelena@gmail.com>
2016-05-09 19:56:50 +0200

    Add files via upload

---
+++ app/mock-heroes.js
@@ -0,0 +1,18 @@
+System.register([], function(exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
+    var HEROES;
+    return {
+        setters:[],
+        execute: function() {
+            exports_1("HEROES", HEROES = [
+                { "id": "img/lampa.jpg", "name": "HOME", "menu1": "PERSONAL" },
+                { "id": "img/projects.jpg", "name": "PROJECTS", "menu1": "HOME" },
+                { "id": "img/timeline.jpg", "name": "STUDIO", "menu1": "2015" },
+                { "id": "img/timeline.jpg", "name": "NEWS", "menu1": "STUDIO LAB" },
+                { "id": "img/fiji.jpg", "name": "CONTACT", "menu1": "Stanga" },
+            ]);
+        }
+    }
+});
+//# sourceMappingURL=mock-heroes.js.map
---
+++ app/mock-heroes.ts
@@ -0,0 +1,10 @@
+
+import {Hero} from './hero';
+export var HEROES: Hero[] = [
+    {"id": "img/lampa.jpg", "name": "HOME", "menu1": "PERSONAL"},
+    {"id":"img/projects.jpg", "name": "PROJECTS", "menu1": "HOME"},
+    {"id": "img/timeline.jpg", "name": "STUDIO", "menu1": "2015"},
+    {"id": "img/timeline.jpg", "name": "NEWS", "menu1": "STUDIO LAB"},
+    {"id":"img/fiji.jpg", "name": "CONTACT", "menu1": "Stanga"},
+   
+];
